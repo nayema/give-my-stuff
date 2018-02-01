ce5ae64543f4b7c53f94ce9a80509d3188e9898b
Nayema Nasir <nayema.nasir89@gmail.com>
2018-02-01 14:53:07 -0500

    Add the ability to retrieve all the donation drop boxes in database

--- routes/donation-drop-boxes.spec.js
+++ routes/donation-drop-boxes.spec.js
@@ -1,13 +1,26 @@
 import request from 'supertest'
 
 import app from '../app'
+import db from '../db'
 
 describe('donation drop boxes route', () => {
+  beforeEach(async () => {
+    await db.query('TRUNCATE donation_drop_boxes')
+  })
+
   it('gets all the donation drop boxes', async () => {
+    await db.query(
+      'INSERT INTO donation_drop_boxes (organization_name, latitude, longitude)' +
+      'VALUES (\'Some Organization\', 1.00, -1.00)'
+    )
 
     const response = await request(app)
       .get('/donation-drop-boxes')
 
     expect(response.statusCode).toBe(200)
+    const donationDropBoxes = response.body
+    expect(donationDropBoxes[0]).toHaveProperty('organization_name', 'Some Organization')
+    expect(donationDropBoxes[0]).toHaveProperty('latitude', '1.00')
+    expect(donationDropBoxes[0]).toHaveProperty('longitude', '-1.00')
   })
 })
