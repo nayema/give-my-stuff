3624ea27d4fe6da59f17ac5b1eeb62e7392aef57
Nayema Nasir <nayema.nasir89@gmail.com>
2018-02-21 20:39:13 -0500

    Add github and give my stuff logos

--- client/src/routes/Header.jsx
+++ client/src/routes/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import GithubCorner from 'react-github-corner'
 import Typography from 'material-ui/Typography'
 import { withStyles } from 'material-ui/styles'
+
+import giveMyStuffIconUrl from './give-my-stuff-icon.png'
 
 const styles = () => ({
   root: {
@@ -14,8 +17,16 @@ const styles = () => ({
 })
 
 const Header = ({ classes }) => (
-  <div className={ classes.root }>
-    <Typography type="display3" gutterBottom className={ classes.font }>
+  <div className={classes.root}>
+    <GithubCorner
+      href="https://github.com/nayema/give-my-stuff"
+      bannerColor="#FD6C6C"
+      octoColor="#fff"
+      size={80}
+      direction="right"
+    />
+    <img alt="Give my stuff logo" src={giveMyStuffIconUrl}/>
+    <Typography type="display3" gutterBottom className={classes.font}>
       Give My Stuff
     </Typography>
   </div>
